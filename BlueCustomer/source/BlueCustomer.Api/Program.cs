ea06bc0cc900c35b60343bdfd715eca72047aadf
vmcbaptista <vmcbaptista95@gmail.com>
2022-07-02 22:00:38 +0100

    introduce docker compose and sqlserver

--- BlueCustomer/source/BlueCustomer.Api/Program.cs
+++ BlueCustomer/source/BlueCustomer.Api/Program.cs
@@ -6,13 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddDbContext<BlueContext>(options => options.UseInMemoryDatabase("Blue"));
+builder.Services.AddDbContext<BlueContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Blue")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<BlueContext>>();
+    var context = services.GetRequiredService<BlueContext>();
+
+    logger.LogInformation("Migrating database");
+    context.Database.Migrate();
+    logger.LogInformation("Database migrated");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ BlueCustomer/source/BlueCustomer.Infrastructure/Migrations/BlueContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BlueCustomer.Infrastructure.Migrations;
+public class BlueContextFactory : IDesignTimeDbContextFactory<BlueContext>
+{
+    public BlueContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<BlueContext>();
+        optionsBuilder.UseSqlServer(opts => opts.MigrationsAssembly(typeof(BlueContextFactory).Assembly.FullName));
+
+        return new BlueContext(optionsBuilder.Options);
+    }
+}
